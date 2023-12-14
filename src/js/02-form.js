88520c096ec0de4dbc0a1efbff0e162bbf7228cb
Sviatoslav <113264589+Me1nychuk@users.noreply.github.com>
2023-12-14 18:07:45 +0200

    Add js for second task.

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -1,1 +1,37 @@
 const form = document.querySelector('.feedback-form');
+const inputEmail = form.querySelector('input');
+const inputText = form.querySelector('textarea');
+
+const currentObj = {
+    email:'',
+    message:'',
+} 
+let obj = '';
+const localStorageKey = "feedback-form-state";
+obj = localStorage.getItem(localStorageKey) ?? "";
+if (obj !== "") {
+    currentObj.email = JSON.parse(obj).email;
+     currentObj.message = JSON.parse(obj).message;
+    inputEmail.value = currentObj.email;
+    inputText.value = currentObj.message;
+}
+
+const upLoadNewDate = (event) => {
+    const target = event.target;
+    if (target === inputEmail) {
+        currentObj.email = target.value;
+    } else if(target === inputText) {
+         currentObj.message = target.value;
+    }
+    obj = JSON.stringify(currentObj);
+    localStorage.setItem(localStorageKey,obj)
+};
+form.addEventListener('input',upLoadNewDate)
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(JSON.parse(localStorage.getItem(localStorageKey)));
+    localStorage.removeItem(localStorageKey);
+   form.reset();
+})
